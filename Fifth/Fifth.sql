5fcbb3ab02161f8551d68effc6db56c24ed02c8f
RPostnyy <114684596+RPostnyy@users.noreply.github.com>
2024-05-14 18:44:20 +0400

    Create Fifth.sql

---
+++ Fifth/Fifth.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Users (
+    ID INT PRIMARY KEY,
+    SURNAME VARCHAR(255),
+    DEPARTMENT_ID INT,
+    SALARY DECIMAL(10, 2),
+    FOREIGN KEY (DEPARTMENT_ID) REFERENCES Departments(DEPARTMENT_ID)
+);
+
+INSERT INTO Users (ID, SURNAME, DEPARTMENT_ID, SALARY) VALUES
+(1, 'Алексеев', 3, 50000.00),
+(2, 'Петрухин', 3, 60000.00),
+(3, 'Есенин', 2, 70000.00),
+(4, 'Маяковский', 1, 80000.00),
+(5, 'Нортон', 4, 90000.00),
+(6, 'Антропов', 1, 100000.00),
+(7, 'Андреев', 4, 110000.00),
+(8, 'Силантьев', 1, 120000.00);
